5f3dc0e30f98414e6dc390147bb4b47955c9840c
Irfanbd <gourob83@gmail.com>
2021-11-24 01:17:33 +0600

    Exercise 5
    Write a Java program that keeps a number from the user and generates an integer between 1 and 7 and displays the name of the weekday.

---
+++ src/com/Exercise_5.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.util.Scanner;
+
+public class Exercise_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Number");
+        int day= sc.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Saturday");
+            case 2 -> System.out.println("Sunday");
+            case 3 -> System.out.println("Monday");
+            case 4 -> System.out.println("Tuesday");
+            case 5 -> System.out.println("Wednesday");
+            case 6 -> System.out.println("Thursday");
+            case 7 -> System.out.println("Friday");
+            default -> System.out.println("Invalid Input");
+        }
+
+    }
+}
