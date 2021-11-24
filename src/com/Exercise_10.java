34ac4eb39373046ab8cc8b756637a2b6fa164861
Irfanbd <gourob83@gmail.com>
2021-11-25 01:39:01 +0600

    Exercise 10
    Write a program in Java to display the first 10 natural numbers.

---
+++ src/com/Exercise_10.java
@@ -0,0 +1,13 @@
+package com;
+
+import java.util.Scanner;
+
+public class Exercise_10 {
+    public static void main(String[] args) {
+        System.out.println("First 10 natural number : ");
+        for(int i=1;i<11;i++)
+        {
+            System.out.println(i+"");
+        }
+    }
+}
