00972d4e37d81ede3a931ac481cc766444f5a6d2
Ahmed Irfan <47223579+Irfanbd@users.noreply.github.com>
2022-09-23 21:53:45 +0600

    Update testing.java

--- src/com/testing.java
+++ src/com/testing.java
@@ -1,7 +1,18 @@
 package com;
-
+import java.util.*;
 public class testing {
     public static void main(String args[]){
         System.out.println("Hello");
+        System.out.println("Enter His or Her Age :");
+        Scanner sc = new Scanner(System.in);
+        int age=sc.nextInt();
+        
+        if(age > 18){
+            System.out.println("He/She is Adult");
+        }
+        else{
+            System.out.println("He/She is not Adult");
+        }
+        
     }
 }
