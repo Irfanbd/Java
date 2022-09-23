b7052f336760aa45465e77baac3f830cdf827b20
Ahmed Irfan <47223579+Irfanbd@users.noreply.github.com>
2022-09-23 23:36:43 +0600

    Create prob2.java

---
+++ Conditional_Statement/prob2.java
@@ -0,0 +1,27 @@
+// Write a Java program to solve quadratic equations (use if, else if and else). Go to the editor
+
+//Test Data
+//Input a: 1
+//Input b: 5
+//Input c: 1
+package Conditional_Statement;
+import java.util.*;
+public class prob2 {
+    public static void main(String args[]){
+Scanner input =new Scanner("System.in");
+System.out.println("input a");
+double a= input.nextDouble();
+
+System.out.println("input b");
+double b= input.nextDouble();
+
+System.out.println("input c");
+double c= input.nextDouble();
+
+double result=b*b -4.0*a*c;
+
+if(result>0.0){
+    double r1 =(-b )
+}
+    }
+}
