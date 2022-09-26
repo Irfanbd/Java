9a3e7fd7af1c71465f712fc20529af0a71f19c5f
Ahmed Irfan <47223579+Irfanbd@users.noreply.github.com>
2022-09-27 02:04:17 +0600

    close

--- Conditional_Statement/prob1.java
+++ Conditional_Statement/prob1.java
@@ -1,21 +1,21 @@
 //Write a Java program to get a number from the user and print whether it is positive or negative
 package Conditional_Statement;
-import java.util.*;
-public class prob1 {
-    public static void main ( String args[]){
-        System.out.println("Input a number");
-        Scanner sc =new Scanner(System.in);
-        int num =sc.nextInt();
 
-        if(num>0){
+import java.util.*;
+
+public class prob1 {
+    public static void main(String args[]) {
+        System.out.println("Input a number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num > 0) {
             System.out.println("Positive Number");
-        }
-        else if(num <0){
+        } else if (num < 0) {
             System.out.println("Number is Negative");
-        }
-        else{
+        } else {
             System.out.println("The number is zero");
         }
-
+        sc.close();
     }
 }
--- Conditional_Statement/prob2.java
+++ Conditional_Statement/prob2.java
@@ -5,30 +5,33 @@
 //Input b: 5
 //Input c: 1
 package Conditional_Statement;
+
 import java.util.Scanner;
+
 public class prob2 {
-    public static void main(String args[]){
-Scanner input =new Scanner("System.in");
-System.out.println("input a");
-double a= input.nextDouble();
+    public static void main(String args[]) {
+        Scanner input = new Scanner("System.in");
+        System.out.println("input a");
+        double a = input.nextDouble();
 
-System.out.println("input b");
-double b= input.nextDouble();
+        System.out.println("input b");
+        double b = input.nextDouble();
 
-System.out.println("input c");
-double c= input.nextDouble();
+        System.out.println("input c");
+        double c = input.nextDouble();
 
-double result=b*b -4.0*a*c;
+        double result = b * b - 4.0 * a * c;
 
-if(result>0.0){
-    double r1 =(-b+Math.pow(result,0.5))/(2.0*a);
-    double r2=(b+Math.pow(result,0.5))/(2.0*a);
-}
-else if (result == 0.0) {
-    double r1 = -b / (2.0 * a);
-    System.out.println("The root is " + r1);
-} else {
-    System.out.println("The equation has no real roots.");
-}
+        if (result > 0.0) {
+            double r1 = (-b + Math.pow(result, 0.5)) / (2.0 * a);
+            double r2 = (b + Math.pow(result, 0.5)) / (2.0 * a);
+            System.out.println("The Roots Are:" + r1 + "and" + r2);
+        } else if (result == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        } else {
+            System.out.println("The equation has no real roots.");
+        }
+        input.close();
     }
 }
